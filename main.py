c5d01a9596bacf416a0952da53df889f13a1a090
LevDergilev <dergilev.lev@gmail.com>
2025-06-28 13:33:57 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import telebot
+from tokenid import token
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+        bot.reply_to(message, "Привет! Я EcoLev_Bot, Я Буду Отвечать На Твои Вопросы По Экологии. Подробности: Напиши Команду: /help")
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+        bot.reply_to(message, "Вопросы: /ecology_what_is_it, /global_warming_what_is_it, /how_to_respect_the_ecology, /")
+
+@bot.message_handler(commands=['ecology_what_is_it'])
+def send_welcome(message):
+        bot.reply_to(message, "Экология — это познание экономики природы, одновременное исследование всех взаимоотношений живого с органическими и неорганическими компонентами окружающей среды… Одним словом, экология — это наука, изучающая все сложные взаимосвязи в природе, рассматриваемые Дарвином как условия борьбы за существование.")
+
+@bot.message_handler(commands=['global_warming_what_is_it'])
+def send_welcome(message):
+        bot.reply_to(message, "Глобальное потепление — распространенное название изменения климата, которое идет уже около двух веков. ООН определяет это явление как долгосрочные изменения температуры и погодных условий, вызванное естественными причинами, вроде активности Солнца, и антропогенными — из-за действий человека.")
+
+@bot.message_handler(commands=['how_to_respect_the_ecology'])
+def send_welcome(message):
+        bot.reply_to(message, "Чтобы соблюдать экологию, нужно минимизировать воздействие на окружающую среду, начиная с простых повседневных действий. Это включает в себя сокращение отходов, разумное потребление ресурсов, таких как вода и электроэнергия, и заботу о природе во время отдыха на природе.")
+
+@bot.message_handler(commands=[''])
+def send_welcome(message):
+        bot.reply_to(message, "")
+
+bot.polling()
